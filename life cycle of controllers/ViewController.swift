e0dab54ad374dc71be1e7e9d57194b8038edc87c
NewUser <sidoruk.misha@gmail.com>
2018-12-12 23:25:46 +0300

    Initial Commit

---
+++ life cycle of controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  life cycle of controllers
+//
+//  Created by Michael Sidoruk on 12/12/2018.
+//  Copyright © 2018 Michael Sidoruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
